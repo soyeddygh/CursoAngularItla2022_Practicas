5a79f42e9a30bac5ee3fbf3f0b10c497bd1b31df
Edison Guzman <55853838+soyeddygh@users.noreply.github.com>
2022-03-21 19:03:03 -0400

    estilo

--- Angular_itla/FinanzasPersonales/FinanzasPersonales/src/app/transacciones/listado/listado.component.ts
+++ Angular_itla/FinanzasPersonales/FinanzasPersonales/src/app/transacciones/listado/listado.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AppService } from 'src/app/app.service';
 import { PresupuestoService } from 'src/app/presupuesto/services/presupuesto.service';
+import { Tipo } from 'src/model/Tipo';
 import { Transaccion } from 'src/model/transaccion';
 
 @Component({
@@ -12,15 +13,16 @@ export class ListadoComponent implements OnInit {
 
   public opcionTran: string ='';
   public transacciones: Transaccion[] = [];
+  public tipos: Tipo[] = [];
 
   constructor(public preService: PresupuestoService, public appService: AppService) { }
 
   ngOnInit(): void {
-    this.preService.obtenerTipos();
+    this.tipos = this.preService.obtenerTipos();
     console.log(this.appService.obtenerRegistros());
   }
 
-  filtrar()
+  public filtrar()
   {
     this.transacciones = this.appService.obtenerRegistros()
                .filter(e => e.tipo == this.opcionTran);
